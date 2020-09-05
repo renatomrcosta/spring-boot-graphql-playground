d98bd6f1684a17a5567b8c30462ac04ab21a136d
Renato Costa <renato.costa@jimdo.com>
2020-09-05 13:59:57 +0200

    added tracing

--- src/main/kotlin/com/xunfos/graphqlplayground/GraphqlPlaygroundApplication.kt
+++ src/main/kotlin/com/xunfos/graphqlplayground/GraphqlPlaygroundApplication.kt
@@ -1,10 +1,15 @@
 package com.xunfos.graphqlplayground
 
+import graphql.execution.instrumentation.tracing.TracingInstrumentation
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class GraphqlPlaygroundApplication
+class GraphqlPlaygroundApplication {
+    @Bean
+    fun tracing() = TracingInstrumentation()
+}
 
 fun main(args: Array<String>) {
     runApplication<GraphqlPlaygroundApplication>(*args)
